52b37247a6ad708d783cfd7f2b9b4f9b33f87ea9
ozdogru1 <73661358+ozdogru1@users.noreply.github.com>
2022-08-22 22:55:46 +0300

    rating svg added

--- src/Component/Filter.js
+++ src/Component/Filter.js
@@ -45,9 +45,11 @@ const Filter = ({
                   handleSelectedRating(rtg.value)
                    
                 }}
-                  className={rating === rtg.value ? "bg-sky-500 rounded text-white p-1" : ""}
+                  className={rating === rtg.value ? "bg-sky-500 rounded flex items-center text-white p-1" : "flex items-center"}
                 >
                   {rtg.label}
+                  <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+
                 </button>
               </div>
             ))}
@@ -71,7 +73,7 @@ const Filter = ({
                   value=""
                   checked={selectedCheckBoxId.includes(item.id)}
                   onChange={() => handleChangePlaceChecked(item.id)}
-                  className="w-4 h-4 text-blue-600 bg-black rounded border-gray-600 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  className="w-4 h-4 text-blue-600 bg-black rounded border-gray-600 focus:ring-blue-500 "
                 />
               </div>
             ))}
@@ -84,7 +86,7 @@ const Filter = ({
             <div>
               <label
                 htmlFor="min"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                className="block mb-2 text-sm font-medium text-gray-900  "
               >
                 Min
               </label>
@@ -92,13 +94,13 @@ const Filter = ({
                 type="number"
                 value={min}
                 onChange={handleMin}
-                className="bg-gray-50 border w-24 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-50 border w-24 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 "
               />
             </div>
             <div>
               <label
                 htmlFor="max"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                className="block mb-2 text-sm font-medium text-gray-900 "
               >
                 Max
               </label>
@@ -106,12 +108,12 @@ const Filter = ({
                 type="number"
                 onChange={handleMax}
                 value={max}
-                className="bg-gray-50 border w-24 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-50 border w-24 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5"
               />
             </div>
           </div>
         </div>
-        <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-2 mt-3 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" onClick={clearAllFilter}> clear all filter</button>
+        <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-2 mt-3 a" onClick={clearAllFilter}> clear all filter</button>
       </div>
 
     </div>
